5a623d1d39d9dc4e8629a8f23c4ed44fce9e7cac
Tselmeg12 <dr.tse12@gmail.com>
2020-07-12 03:49:23 -0500

    bug v2.2
    sad

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import data from './data';
 
 const app=express();
 app.get ("/api/product", (req, res)=>{
-    res.send(data.product);
+    res.send(data.products);
 });
-0app.listen (5000,()=>{ console.log("Server started at http://localhost:5000")});
+app.listen (5000,()=>{ console.log("Server started at http://localhost:5000")});
 
