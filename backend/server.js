385f2c58a9a06f15f72f052cfb11079c3e8bd46f
TukyBoy007 <tcoreboy@gmail.com>
2020-07-12 17:34:21 +0800

    V.2.3.4
    Axios BUG

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import data from './data';
 
 const app=express();
 
-app.get ("/api/product", (req, res)=>{
+app.get ("/api/products", (req, res)=>{
     res.send(data.products);
 });
 app.listen (5000,()=>{ console.log("Server started at http://localhost:5000")});
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom'
-import data from '../data';
+import axios from 'axios';
 
 function HomeScreen(props) {
+
+  const [products, setPRoduct] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () =>{
+      const {data} = await axios.get("/api/products");
+      setPRoduct(data);
+    }
+    return () =>{
+      //
+    };
+  }, [])
+  
   return <ul className="products">
     {
-      data.products.map(product =>
+      products.map(product =>
         <li>
           <div className="product">
             <Link to={'/product/' + product._id}>
