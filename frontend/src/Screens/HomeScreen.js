0f8d22deb0ddf8f7d8dc9b5c4f3644ad356dc3e4
Tselmeg12 <dr.tse12@gmail.com>
2020-07-12 02:02:24 -0500

    new one

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import data from '../data';
-function HomeScreen (props){
-    return   <ul className="products">
+
+function HomeScreen(props) {
+  return <ul className="products">
     {
-      
-      data.product.map(product => 
+      data.products.map(product =>
         <li>
-      <div className="product">
-      <img className="product-image" src={product.image}alt="product"></img>
-      <div className="product-name"></div>
-        <Link to ={'/product'+product._id}>{product.name}</Link>
-      </div>
-      <div className="product-brand">{product.brand}</div>
-      <div className="product-price">${product.price}</div>
-      <div className="product-rating">{product.rating} Starts(10 {product.numReviews})</div>
-  </li>)
-}              
- </ul>
+          <div className="product">
+            <Link to={'/product/' + product._id}>
+              <img className="product-image" src={product.image} alt="product" />
+
+            </Link>
+            <div className="product-name">
+              <Link to={'/product/' + product._id}>{product.name}</Link>
+            </div>
+            <div className="product-brand">{product.brand}</div>
+            <div className="product-price">${product.price}</div>
+            <div className="product-rating">{product.rating} Stars ({product.numReiews} Reviews)</div>
+          </div>
+        </li>)
+    }
+
+
+
+  </ul>
 }
-export default HomeScreen; 
+export default HomeScreen;
