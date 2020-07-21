83ef37d4a8c90ba514444dbe93c6d8744a732104
Tselmeg12 <dr.tse12@gmail.com>
2020-07-21 02:44:10 -0500

    fake one

--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -19,9 +19,9 @@ const HomeHeader = props => (
 
     <div className="header-container">
       <div className="content">
-        <h1>I love carpet. I love desk.</h1>
+        <h1>Хамгийн хурдан Хамгийн шуурхай.</h1>
         <Link to="/products" className="btn">
-          I love lamp
+        Бараа Захиалах
         </Link>
       </div>
     </div>
