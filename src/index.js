057ab1b2f8e8aec8691a27ce6317904fdb494a49
TukyBoy007 <tcoreboy@gmail.com>
2020-07-21 16:22:31 +0800

    NEW VERSION
    1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-import store, { history } from './store';
-import App from './components/App';
+import "assets/scss/material-kit-react.scss?v=1.9.0";
 
-import './index.css';
+// pages for this product
+import Components from "views/Components/Components.js";
+import LandingPage from "views/LandingPage/LandingPage.js";
+import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
 
-const target = document.getElementById('root');
+var hist = createBrowserHistory();
 
-render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <App />
-      </div>
-    </ConnectedRouter>
-  </Provider>,
-  target
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/" component={Components} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
